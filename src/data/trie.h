ab8336ce5a94e5e73dea502312317e465b8fd6e2
Arxis75 <a.b@c.com>
2025-10-22 09:57:52 +0200

    +

--- src/data/trie.h
+++ src/data/trie.h
@@ -1,34 +1,42 @@
 struct node
 {
+    public:
+        void add(const ByteSet<> &key, const ByteSet<> &value)
+        {
+            if(m_children.size() == 0) {
+                node* n = new node(key, value);
+            }     
+        }
+
+        virtual ByteSet<> getHash() const 
+        {
+            ByteSet<> to_hash;
+            for(int i=0;i<m_children.size(); i++)
+                to_hash.push_back(m_children[i]->getHash());
+            return to_hash.keccak256();
+        }
+
     protected:
-        node();
+        node(const ByteSet<> &key, const ByteSet<> &value);
+    
+    private:
+        node* m_parent;
+        std::map<ByteSet<> key, node* child> m_children;
+        ByteSet<> m_value;
 };
 
-struct Trie
+struct Trie : public node
 {
-    node* m_root;
-
-    void add(ByteSet<> key, ByteSet<> value);
 };
 
 struct BrancheNode : public node
 {
-    ByteSet<> m_hash;
-    ByteSet<> m_value;
-    node* m_parent;
-    std::map<char, node*> m_children;
 };
 
 struct ExtensionNode : public node
 {
-    ByteSet<> m_hash;
-    ByteSet<> m_value;
-    node* m_parent;
-    std::map<char, node*> m_children;
 };
 
 struct LeafNode : public node
 {
-    ByteSet<> m_value;
-    node* m_parent;
 };
